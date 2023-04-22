336bcde6bc16de4df52eeafb12347da7ab8bbee9
yangjin <yangjin.im@bytedance.com>
2023-04-22 19:38:56 +0800

    feat: add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Nunito } from 'next/font/google';
 import './globals.css'
+import Navbar from './components/navbar/Navbar';
 
 export const metadata = {
   title: 'Airbnb',
@@ -17,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
